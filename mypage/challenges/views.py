d152587a4e5598eaca05359fcd0511736bb54301
RALHI <ralhi.certification@gmail.com>
2025-06-08 11:43:03 +0530

    create app urls.py

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def challenges(request):
-  return HttpResponse("Welcome to Year 2025 Monthly Challenge")
-
-def challenges_post(request):
-  pass
-
-def challenges_post_description(request):
-  pass
-
+def index(request):
+  return HttpResponse("This Work")
 
