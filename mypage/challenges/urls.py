048aa97bfdea8fc3d69562dfa681517293d12909
RALHI <ralhi.certification@gmail.com>
2025-06-09 21:42:07 +0530

    Adding more Views and URLs

--- mypage/challenges/urls.py
+++ mypage/challenges/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlspatter = [
-  path("january" , views.index)
+urlpatterns = [
+  path("january" , views.index),
+  path("february" , views.february)
 ]
+
--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("This Work")
+  return HttpResponse("Eat no meat for entire month")
+
+def february(request):
+  return HttpResponse("Walk for at least 20 minutes every day")
 
